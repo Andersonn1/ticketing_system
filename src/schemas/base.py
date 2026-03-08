fa66e6fe994a7d85368229cb9a2966fd4493f1cf
Andersonn1 <nathananderson2003@yahoo.com>
2026-03-08 18:51:52 -0400

    Create service ticket schemas

---
+++ src/schemas/base.py
@@ -0,0 +1,30 @@
+"""Base Schema For All Schema Objects"""
+
+from datetime import datetime, timezone
+
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic.alias_generators import to_camel
+
+
+class BaseSchema(BaseModel):
+    """Base Schema Model"""
+
+    model_config = ConfigDict(
+        extra="forbid", alias_generator=to_camel, populate_by_name=True
+    )
+
+
+class AuditMixin:
+    """Audit Mixin"""
+
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        description="When was the entity created",
+    )
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        description="When was the entity last updated",
+    )
+    updated_by: str | None = Field(
+        default=None, description="Who last updated the entity"
+    )
